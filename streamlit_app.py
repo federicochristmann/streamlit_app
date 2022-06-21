0fda6874a586cf845975ab965a25412ab2a22691
Federico Christmann <71786500+federicochristmann@users.noreply.github.com>
2022-06-21 15:14:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My parents new healthy corner')
 streamlit.header('Breakfast menu')
 
 streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie)
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
