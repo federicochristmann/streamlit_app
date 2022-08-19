b1f9c1bdc5f74327603d3b1510ab391f3c5b0abd
Federico Christmann <71786500+federicochristmann@users.noreply.github.com>
2022-08-19 17:07:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 fruit_choice = streamlit.text_imput('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 
