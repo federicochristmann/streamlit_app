8e15511b4e74f6f939db65ca341d4d43006683b9
Federico Christmann <71786500+federicochristmann@users.noreply.github.com>
2022-08-22 12:36:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,3 +67,4 @@ streamlit.dataframe(my_data_rows)
 
 # Set a fruit imput variable (used in the following API call)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
